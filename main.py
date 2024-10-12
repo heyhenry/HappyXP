c96e4afd441fdf180709cf30c9c18e409a81e672
heyhenry <henryyy3@hotmail.com>
2024-10-12 23:54:39 +0800

    created skeleton main file

---
+++ main.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+class MainApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        container = tk.Frame(self)
+        container.pack(side='top', fill='both', expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (SetupPage, LoginPage, EntryPage, HomePage):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky='nswe')
+
+class SetupPage:
+    pass
+
+class LoginPage:
+    pass
+
+class EntryPage:  
+    pass
+
+class HomePage:
+    pass
