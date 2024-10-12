d68b59d3ba5191dbbd395f0c18c3244a9679b08d
heyhenry <henryyy3@hotmail.com>
2024-10-13 06:45:09 +0800

    implemented load_user func

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ class MainApp(tk.Tk):
 
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-        
-        self.check_user_exists()
 
         self.frames = {}
 
         self.geometry('1000x1000')
+
+        self.load_user()
 
         # loop that puts all the pages (aka frames) into a dictionary called frames
         for F in (SetupPage, LoginPage, EntryPage, HomePage):
@@ -42,11 +42,12 @@ class MainApp(tk.Tk):
             }
         return obj
 
-    def check_user_exists(self):
-        if not users:
-            user = UserInfo('', '')
-            users['user'] = user
-            self.update_user_save()
+    def load_user(self):
+        if os.path.exists(user_savefile):
+            with open(user_savefile, 'r') as file:
+                user_data = json.load(file)
+                for user, user_value in user_data.items():
+                    users[user] = UserInfo(user_value['username'], user_value['password'])
 
     def update_user_save(self):
         json_object = json.dumps(users, indent=4, default=self.custom_serializer)
@@ -95,25 +96,39 @@ class LoginPage(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
-    def create_page(self):
-        setup_window = tk.Frame(self, highlightbackground='black', highlightthickness=2)
-        page_title = tk.Label(setup_window, text='Login To HappyXP', font=('Kozuka Mincho Pro M', 48))
-        username_title = tk.Label(setup_window, text='Username:', font=('Kozuka Mincho Pro M', 24))
-        username = tk.Entry(setup_window, textvariable=self.username_var, font=('Kozuka Mincho Pro M', 24))
-        password_title = tk.Label(setup_window, text='Password:', font=('Kozuka Mincho Pro M', 24))
-        password = tk.Entry(setup_window, textvariable=self.password_var, font=('Kozuka Mincho Pro M', 24))
-        submit = tk.Button(setup_window, text='Create', font=('Kozuka Mincho Pro M', 24))
+        self.username_var = tk.StringVar()
+        self.password_var = tk.StringVar()
 
-        setup_window.place(relx=0.5, rely=0.5, anchor='center')
-        setup_window.propagate(0)
-        setup_window.config(width=800, height=800)
-        page_title.place(x=200, y=100)
+        self.create_page()
+
+    def create_page(self):
+        login_window = tk.Frame(self, highlightbackground='black', highlightthickness=2)
+        page_title = tk.Label(login_window, text='Login To HappyXP', font=('Kozuka Mincho Pro M', 48))
+        username_title = tk.Label(login_window, text='Username:', font=('Kozuka Mincho Pro M', 24))
+        username = tk.Entry(login_window, textvariable=self.username_var, font=('Kozuka Mincho Pro M', 24))
+        password_title = tk.Label(login_window, text='Password:', font=('Kozuka Mincho Pro M', 24))
+        password = tk.Entry(login_window, textvariable=self.password_var, font=('Kozuka Mincho Pro M', 24))
+        submit = tk.Button(login_window, text='Create', font=('Kozuka Mincho Pro M', 24), command=self.process_login)
+        self.error_message = tk.Label(login_window, font=('Kozuka Mincho Pro M', 24), foreground='red')
+
+        login_window.place(relx=0.5, rely=0.5, anchor='center')
+        login_window.propagate(0)
+        login_window.config(width=800, height=800)
+        page_title.place(x=150, y=100)
         username_title.place(x=225, y=250)
         username.place(x=225, y=300)
         password_title.place(x=225, y=450)
         password.place(x=225, y=500)
         submit.place(x=300, y=600, width=200)
+        self.error_message.place(x=250, y=650)
 
+    def process_login(self):
+        self.error_message.config(text='')
+        
+        if self.username_var.get() == users['user'].username and self.password_var.get() == users['user'].password:
+            self.controller.show_page(HomePage)
+        else:
+            self.error_message.config(text='Incorrect Username or Password. Try again.')
 
 class EntryPage(tk.Frame):  
     def __init__(self, parent, controller):
