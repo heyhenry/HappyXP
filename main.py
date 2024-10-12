8f80f8ab6b041f69cce9a3bd66ec56b9f4d03eb2
heyhenry <henryyy3@hotmail.com>
2024-10-13 05:54:22 +0800

    user implemented with funcs + serializer

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import tkinter as tk
+from user import UserInfo
+import json
+import os
+
+users = {}
+user_savefile = 'user_save.json'
 
 class MainApp(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -10,6 +16,8 @@ class MainApp(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         
+        self.check_user_exists()
+
         self.frames = {}
 
         self.geometry('1000x1000')
@@ -25,6 +33,25 @@ class MainApp(tk.Tk):
     def show_page(self, page):
         frame = self.frames[page]
         frame.tkraise()
+
+    def custom_serializer(self, obj):
+        if isinstance(obj, UserInfo):
+            return {
+                'username': obj.username,
+                'password': obj.password
+            }
+        return obj
+
+    def check_user_exists(self):
+        if not users:
+            user = UserInfo('', '')
+            users['user'] = user
+            self.update_user_save()
+
+    def update_user_save(self):
+        json_object = json.dumps(users, indent=4, default=self.custom_serializer)
+        with open(user_savefile, 'w') as outfile:
+            outfile.write(json_object)
 
 class SetupPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -54,6 +81,9 @@ class SetupPage(tk.Frame):
         password_title.place(x=225, y=450)
         password.place(x=225, y=500)
         submit.place(x=300, y=600, width=200)
+    
+    # no need for restrictions on input data as its personal use (atleast for now)
+        
 
 class LoginPage(tk.Frame):
     def __init__(self, parent, controller):
---
+++ user.py
@@ -0,0 +1,5 @@
+class UserInfo():
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
