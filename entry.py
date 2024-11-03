3cca9f9ae385f3521d735493a1b779f96f833a64
heyhenry <henryyy3@hotmail.com>
2024-11-04 07:58:39 +0800

    created the entry class object

---
+++ entry.py
@@ -0,0 +1,10 @@
+class Entry:
+    def __init__(self, title, content_type, rating, current_progess, total_progress, status, start_date, end_date):
+        self.title = title
+        self.content_type = content_type
+        self.rating = rating
+        self.current_progress = current_progess
+        self.total_progress = total_progress
+        self.status = status
+        self.start_date = start_date
+        self.end_date = end_date
--- user.py
+++ user.py
@@ -1,4 +1,4 @@
-class UserInfo():
+class UserInfo:
     def __init__(self, display_name, username, password, toggle_login, bio_message):
         self.display_name = display_name
         self.username = username
